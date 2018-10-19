ac9f3a85ea7ee666ea1b9850546cf81e2ca1322c
Falk TANNHÄUSER <falk.tannhauser@free.fr>
2018-10-19 08:53:56 +0200

    Check if PyTorch is installed in environment

--- lib/cygwin_make.sh
+++ lib/cygwin_make.sh
@@ -1,7 +1,8 @@
 # Compilation under Cygwin:
-# Usage:    ./cygwin_make.sh [python_virtual_environment]
+# Usage:    ./cygwin_make.sh [python_environment]
 #   - Check if 'python' command refers to Anaconda Python, rather than Cywin's
 #     If necessary, activate Anaconda requested virtual environment
+#   - Check if PyTorch is installed
 #   - Set the environment variables for Visual C++ compiler (using Windows command shell for calling .bat file)
 #   - If successful ('cl' is accessible), then launch make.sh (using Cygwin Bash)
 if ! type python | grep -i conda ; then
@@ -14,6 +15,12 @@ if ! type python | grep -i conda ; then
     fi
 fi
 type python
+if ! python -c 'import torch ; print("PyTorch version:", torch.__version__)' ; then
+    echo "PyTorch not installed in your Anaconda Python environment :-("
+    echo "If it has been installed in a virtual environment, pass the environment's name as command line argument, e.g."
+    echo "    $0 py_with_torch"
+    exit 1
+fi
 
 VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
 BASH_FROM_WINDOWS=$(cygpath -w $(type -p bash))
